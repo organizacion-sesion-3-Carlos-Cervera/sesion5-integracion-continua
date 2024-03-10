8d746f218a8b2e7d5b97b9c470c1b7a42aed733d
Carlos Cervera <c.cerveratortosa@edu.gva.es>
2024-03-10 22:24:00 +0100

    Adición tests

--- test/par-spec.js
+++ test/par-spec.js
@@ -11,16 +11,20 @@ describe('Módulo par', function () {
             assert.equal(math.esPar(1), "impar");
             assert.equal(math.esPar(2), "par");
             assert.equal(math.esPar(3), "impar");
+            assert.equal(math.esPar(8), "par");
         });
         it('Numeros pares del 10 al 100', function () {
             assert.equal(math.esPar(21), "impar");
             assert.equal(math.esPar(36), "par");
             assert.equal(math.esPar(80), "par");
+            assert.equal(math.esPar(95), "impar");
         });
         it('Numeros pares negativos', function () {
             assert.equal(math.esPar(-16), "par");
             assert.equal(math.esPar(-61), "impar");
             assert.equal(math.esPar(-77), "impar");
+            assert.equal(math.esPar(-86), "par");
+
         });
     });
 });
