776f2536d956514cd28e58e639b999b98e57db20
Carlos Cervera <c.cerveratortosa@edu.gva.es>
2024-03-10 21:45:52 +0100

    Test números pares

---
+++ par.js
@@ -0,0 +1,10 @@
+function esPar(a) {
+    if (a%2 == 0)
+        return "par";
+    else
+        return "impar"
+}
+
+module.exports = {
+    esPar
+}
---
+++ test/par-spec.js
@@ -0,0 +1,26 @@
+// Librerías para testeo
+const { assert } = require('chai');
+
+// Carga del programa principal
+const math = require('../par');
+
+// Tests
+describe('Módulo par', function () {
+    describe('Función es par', function () {
+        it('Numeros pares del 1 al 10', function () {
+            assert.equal(math.esPar(1), "impar");
+            assert.equal(math.esPar(2), "par");
+            assert.equal(math.esPar(3), "impar");
+        });
+        it('Numeros pares del 10 al 100', function () {
+            assert.equal(math.esPar(21), "impar");
+            assert.equal(math.esPar(36), "par");
+            assert.equal(math.esPar(80), "par");
+        });
+        it('Numeros pares negativos', function () {
+            assert.equal(math.esPar(-16), "par");
+            assert.equal(math.esPar(-61), "impar");
+            assert.equal(math.esPar(-77), "impar");
+        });
+    });
+});
